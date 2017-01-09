276837766c555edba6249c8fec2f2a9b950124f1
Marculet Catalin <catalinmarculet@users.noreply.github.com>
2017-01-09 21:45:32 +0200

    Update Data Structures and Constants.h

--- Data Structures and Constants.h
+++ Data Structures and Constants.h
@@ -8,7 +8,12 @@
 #define INCREASE 1
 #define STAND 0
 #define NO_DIRECTIONS 4 
+
 #define SQUARE_SIDE 25
+#define DIM_OF_WIN_OX 900
+#define DIM_OF_WIN_OY 700
+#define DIM_OF_BUTTON_OX 230
+#define DIM_OF_BUTTON_OY 120
 
 struct matrixPosition
 {
@@ -27,7 +32,7 @@ char possibleMoves[] = { 'u', 'r', 'd', 'l' };
 
 int quoridorMatrix[NO_LIN_COL + 1][NO_LIN_COL + 1];
 
-matrixPosition movementArray[ NO_DIRECTIONS ];
+matrixPosition movementArray[NO_DIRECTIONS];
 
 player firstPlayer;
 player secondPlayer;
