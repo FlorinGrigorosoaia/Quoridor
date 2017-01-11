d0afbb891439cbb1ced0ebbc5cd0b73869bc7e76
Marculet Catalin <catalinmarculet@users.noreply.github.com>
2017-01-12 00:22:32 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,21 @@
+
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include "Data Structures and Constants.h"
-#include "Functions.h"
+#include "Input and Output.h"
 
-using namespace std;
 
-int playingMode;
 
 int main()
 {
-	initializeMovementArray( movementArray );
-	cout << " Pentru a juca in modul vs CPU apasati '0' + ENTER : " << '\n';
-	cout << " Pentru a juca in modul 1 vs 1 apasati '1' + ENTER : " << '\n';
-	cout << " Pentru a juca in modul 2 vs 2 apasati '2' + ENTER : " << '\n';
-	cin >> playingMode;
+	buildColors();
+	initializeMovementArray(movementArray);
+	buildButtons();
+	buildTexts();
+	buildRectangles();
+	buildVerticalWalls();
+	buildHorizontalWalls();
 
-	if ( playingMode == 1 )
-		play1vs1();
+	startMenu();
+
 	return 0;
 }
